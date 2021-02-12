dfa5b7b99802975332d5d76ea9ccc6373d7e4145
Kevin Phan <ken.phan@live.ca>
2021-02-12 04:06:31 +0000

    HTTPS Request, API, JSON.parse

---
+++ breedFetcher.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const args = process.argv.slice(2);
+const breedQuery = `?q=${args[0]}`;
+
+const url = `https://api.thecatapi.com/v1/breeds/search`+breedQuery;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+    return;
+  }
+  const data = JSON.parse(body);
+  if (data.length < 1) {
+    console.log("Could not find breed based on input")
+  } else {
+    console.log(data[0].description);
+  }
+  });
