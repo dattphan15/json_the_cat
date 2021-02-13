15d9b37b0ce62a857fe6c7d0c43263c108226488
Kevin Phan <ken.phan@live.ca>
2021-02-13 21:06:37 +0000

    refactored

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const { fetchBreedDescription } = require('./breedFetcher');
-
-// const args = process.argv.slice(2);
-// const breedQuery = `${args[0]}`;
 const breedName = process.argv[2];
 
-
 fetchBreedDescription(breedName, (error, description) => {
-  
   
   if (error) {
     console.log('Error fetch details:', error);
   } else {
     console.log(description);
   }
-
 
 });
